cd3779f0424f0ee174f7e905400703272370e69a
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2022-07-23 22:32:22 +0530

    Create "Hello World!" in C.c
    n this challenge, we will learn some basic concepts of C that will get you started with the language. You will need to use the same syntax to read input and write output in many C challenges. As you work through these problems, review the code stubs to learn about reading from stdin and writing to stdout.

---
+++ "Hello World!" in C.c
@@ -0,0 +1,16 @@
+/*This challenge requires you to print  on a single line, and then print the already provided input string to stdout. If you are not familiar with C, you may want to read about the printf() command.*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char s[100];
+    gets(s);
+    puts("Hello, World!");
+    puts(s);    
+    return 0;
+}
