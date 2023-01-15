c0d682999b138798db69ecc651a5a787c5675df8
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-01-15 19:45:56 +0530

    Create Substring Comparison.java
    We define the following terms:
    
    Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
    For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
    
    A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
    Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .

---
+++ Substring Comparison.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String substring = s.substring(0,k);
+        String smallest = substring;
+        String largest = substring;
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+       
+        for(int i=1;i<=s.length()-k;i++){
+            substring = s.substring(i,i+k);
+            if(substring.compareTo(smallest)<0){
+                smallest=substring;
+            }
+            
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        if(substring.compareTo(largest)>0){
+                largest=substring;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
